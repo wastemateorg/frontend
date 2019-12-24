7d52c0b7a75c86b04f3b6a16d62c0f4754d0e6cd
fillipi rocha <fillipirocha@fillipis-MacBook-Pro.local>
2019-12-24 15:22:49 -0800

    formated code

--- src/Components/Dashboard/LeftPanel/PhotoList/index.js
+++ src/Components/Dashboard/LeftPanel/PhotoList/index.js
@@ -1,23 +1,41 @@
 import React, { Component } from "react";
-import PhotoComponent from './photoComponent';
-import img1 from './img/trash.png';
-import img2 from './img/trash2.jpg';
-import img3 from './img/trash3.jpg';
-import img4 from './img/trash4.jpg'
+import PhotoComponent from "./photoComponent";
+import img1 from "./img/trash.png";
+import img2 from "./img/trash2.jpg";
+import img3 from "./img/trash3.jpg";
+import img4 from "./img/trash4.jpg";
 
 class PhotoList extends Component {
-
-    render(){
-        return (
-            <div>
-            <PhotoComponent title='San Jose' subtitle='10 west ave' details='trash will be after the bridge on the left' image={img1}/>
-            <PhotoComponent title='Berkeley' subtitle='103 3rd st' details='trash has been slowly pilling up over the past week' image={img2}/>
-            <PhotoComponent title='San Jose' subtitle='465 1st ave' details='no details' image={img3}/>
-            <PhotoComponent title='San Mateo' subtitle='177 west brook' details='Trash has been attracting rats, please help!' image={img4}/>
-            </div>
-
-            )
-    }
+  render() {
+    return (
+      <div>
+        <PhotoComponent
+          title="San Jose"
+          subtitle="10 west ave"
+          details="trash will be after the bridge on the left"
+          image={img1}
+        />
+        <PhotoComponent
+          title="Berkeley"
+          subtitle="103 3rd st"
+          details="trash has been slowly pilling up over the past week"
+          image={img2}
+        />
+        <PhotoComponent
+          title="San Jose"
+          subtitle="465 1st ave"
+          details="no details"
+          image={img3}
+        />
+        <PhotoComponent
+          title="San Mateo"
+          subtitle="177 west brook"
+          details="Trash has been attracting rats, please help!"
+          image={img4}
+        />
+      </div>
+    );
+  }
 }
 
 export default PhotoList;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
-import axios from 'axios';
 import { Provider } from 'react-redux';
 import rootReducer from './Redux/Reducers/rootReducer'
 
